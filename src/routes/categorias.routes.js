2d82543a02a77b43683e0e47f6b5a6e617808756
yuliana669 <yulianacarrilloaza@gmail.com>
2025-05-25 18:11:48 -0500

    Delete src directory

--- src/routes/categorias.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { obtenerCategorias } from "../controllers/categoria.controllers.js";
-
-const router =express.Router();
-
-router.get("/all", obtenerCategorias);
-
-export default router;
